0812cc323f283070a6905e43bcb2ae78c9f5ad82
Lukas Kurz <vb.net@kurzweb.de>
2021-10-17 18:10:07 +0200

    Fix "bad" .gitignore

---
+++ MC Manager/Logs/LogInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace MC_Manager.Logs
+{
+    public class LogInfo
+    {
+        public LogInfo(StorageFile file)
+        {
+            this.File = file;
+        }
+
+        public string Name { get => this.File.DisplayName; }
+        public StorageFile File { get; private set; }
+
+        public string Content
+        {
+            get
+            {
+                using (IRandomAccessStream fileStream = this.File.OpenSync(FileAccessMode.Read))
+                using (Stream stream = fileStream.AsStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd().Replace("\n", "\r\n");
+                }
+            }
+        }
+    }
+}
