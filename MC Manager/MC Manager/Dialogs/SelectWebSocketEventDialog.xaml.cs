5eb385077a73b5986d8ecb72b8c199d6843dd019
Lukas Kurz <lukas@shortdev.de>
2022-08-16 10:37:15 +0200

    Dialog

---
+++ MC Manager/MC Manager/Dialogs/SelectWebSocketEventDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using ShortDev.Minecraft.Bedrock.McWebSocket.PackagePayloads;
+
+namespace MC_Manager.Dialogs
+{
+    public sealed partial class SelectWebSocketEventDialog : ContentDialog
+    {
+        public SelectWebSocketEventDialog()
+        {
+            this.InitializeComponent();
+
+            EventSelectComboBox.ItemsSource = SubscribePackage.WellKnownEvents;
+        }
+    }
+}
